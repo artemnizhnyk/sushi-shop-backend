9385d7760fe6fd7b60e238c7ee3ed8f88b52a260
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-21 10:45:28 +0200

    initial commit

---
+++ src/main/java/com/artemnizhnyk/sushishopbackend/SushiShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.sushishopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushiShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemnizhnyk/sushishopbackend/SushiShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.sushishopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
